656db0ae527135927efc6066e106ecbd6554af77
Josep Servat <jservat@outlook.es>
2022-03-20 00:40:39 +0100

    fix test

--- test/e2e/services/binanceService.test.ts
+++ test/e2e/services/binanceService.test.ts
@@ -2,7 +2,7 @@ import { getCurrentPrice } from '../../../src/services/binanceService';
 
 describe('Binance', () => {
   it('should get current price', async () => {
-    const price = await getCurrentPrice('BTCUSDT');
+    const price = await getCurrentPrice('AXSUSDT');
     expect(price).toBeGreaterThan(0);
   });
 })
