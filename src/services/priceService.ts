67c3909f049ae062d1d0f10da732f395dd098124
servatj <jservatlorca@gmail.com>
2023-03-07 01:58:34 +0400

    Fix add binance

--- src/services/priceService.ts
+++ src/services/priceService.ts
@@ -1,12 +1,12 @@
 import ascendexService from "./ascendexService";
-import binanceService from "./binanceService";
+import binancePriceService from "./binancePriceProxy";
 import coindesk from "./coindesk";
 
 export const getCurrentPrice = async (symbol: string, exchange: string) => {
   console.log('get current price', symbol, exchange);
   const exchanges: any = {
     ascendex: ascendexService,
-    binance: binanceService,
+    binance: binancePriceService,
     coindesk: coindesk,
   }
 
