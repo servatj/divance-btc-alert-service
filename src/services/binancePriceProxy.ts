2d04a8a32438c5bddac930b0d1a49641a7f7d836
servatj <jservatlorca@gmail.com>
2023-03-19 11:42:38 +0400

    Fix price divance call

--- src/services/binancePriceProxy.ts
+++ src/services/binancePriceProxy.ts
@@ -1,15 +1,18 @@
-import axios from 'axios';
+import axios, { AxiosError } from "axios";
 
 export const getCurrentPrice = async (symbol: string) => {
-  try {
-   const { data } = await axios.get(`https://price.divance.app:3000/price?pair=${symbol}`);
-   const currentPrice: Number = data.price
-   return Number(currentPrice);
-  } catch (error) {
-    console.log('error getting Current Price', error);
-  }
-}
+	try {
+		const { data } = await axios.get(
+			`https://price.divance.app/price?pair=${symbol}`
+		);
+		const currentPrice: Number = data.price;
+		return Number(currentPrice);
+	} catch (error) {
+    const error1 = error as Error | AxiosError;
+		console.log("error getting Current Price", error1.message);
+	}
+};
 
 export default {
-  getCurrentPrice,
-}
+	getCurrentPrice,
+};
